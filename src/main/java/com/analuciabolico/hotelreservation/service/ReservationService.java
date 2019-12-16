c334a62430445b8b4a996da7ff4837f2ea94ea13
analuciabolico <analuciabolico.dev@gmail.com>
2019-12-16 00:15:14 -0300

    refactor strings

--- src/main/java/com/analuciabolico/hotelreservation/service/ReservationService.java
+++ src/main/java/com/analuciabolico/hotelreservation/service/ReservationService.java
@@ -20,18 +20,18 @@ public class ReservationService implements IReservationService {
     private  static  final Logger logger = LoggerFactory.getLogger(ReservationService.class);
 
     public TypeCustomerEnum getTypeCustomer(String input) {
-        String type = input.substring(0, ':');
+        String type = input.substring(0, input.indexOf(":"));
         return TypeCustomerEnum.isTypeCustomer(type);
     }
 
     public List<LocalDate> getDates(String input) throws ParseException {
         List<LocalDate> dateList = new ArrayList<>();
-        String dates = input.substring(':' + 1);
+        String dates = input.substring(input.indexOf(":") + 1);
         String[] arrayDates = dates.split(Pattern.quote(","));
         for(String date : arrayDates){
             Date dateConvert = new SimpleDateFormat("ddMMMyyyy").parse(date);
             LocalDate localDate = dateConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-            if (!(localDate.getDayOfWeek().toString().toLowerCase().contains(date.substring('(' + 1, ')' - 1)))) {
+            if (!(localDate.getDayOfWeek().toString().toLowerCase().contains(date.substring(date.indexOf("(") + 1, date.indexOf(")") - 1)))) {
                 logger.warn("Day of week is wrong! For the date -> " + date);
             }
             dateList.add(localDate);
